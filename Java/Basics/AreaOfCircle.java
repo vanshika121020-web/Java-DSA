192d07eedb044a85007a245f26f795c72e772651
vanshika121020 <vanshika121020@gmail.com>
2026-09-16 21:52:15 +0530

    Add Area of circle program

---
+++ Java/Basics/AreaOfCircle.java
@@ -0,0 +1,8 @@
+public class AreaOfCircle{
+    public static void main(String[] args){
+        double radius = 3.6;
+        double area = 3.14 * radius * radius;
+        System.out.println("Radius of circle :"+ radius);
+        System.out.println("Area of circle : "  + area);
+    }
+}
