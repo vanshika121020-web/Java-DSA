37ee4f1a499266c3b7d5a6c6ab0b046b3df7ae55
vanshika121020 <vanshika121020@gmail.com>
2026-09-19 13:32:42 +0530

    Add largest of two numbers program

---
+++ Java/Basics/O10_LargestOfTwoNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class O10_LargestOfTwoNumbers{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number : ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter second number : ");
+        int b = sc.nextInt();
+
+        if(a > b){
+            System.out.println("Largest number = " + a );
+        }else{
+            System.out.println("Largest number = " + b );
+        }
+    }
+
+}
+
+
