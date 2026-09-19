2db95b7cacfc7370ec7f15d9ef7da172501752f3
vanshika121020 <vanshika121020@gmail.com>
2026-09-19 13:55:22 +0530

    Add simple interest program

---
+++ Java/Basics/O11_SimpleInterest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class O11_SimpleInterest{
+    public statc void main{
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Principal : ");
+        float p = sc.nextFloat();
+
+        System.out.println("Enter Rate : "):
+        float r = sc.nextFloat();
+
+        System.out.println("Enter Time : ");
+        float t = sc.nextFloat();
+
+        float si = (p * r * t) / 100 ;
+
+        System.out.println("Simple Interest = " + );
+
+
+    }
+}
