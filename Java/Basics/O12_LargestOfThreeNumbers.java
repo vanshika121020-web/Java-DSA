5c051e4b45f9ac10edd2f8891059749a21dd80d7
vanshika121020 <vanshika121020@gmail.com>
2026-09-20 11:17:12 +0530

    Add Largest of three numbers program

---
+++ Java/Basics/O12_LargestOfThreeNumbers.java
@@ -0,0 +1,20 @@
+public class LargestOfThreeNumbers{
+    public static void main(String[] args){
+
+        int a = 12;
+        int b = 23;
+        int c = 15;
+
+        if( a >= b && a >= c ){
+            System.out.println("Largest = " + a );
+
+        }else if( b >= a && b >=c ){
+            System.out.println("Largest = " + b );
+
+        }else{
+            System.out.println("Largest = " + c);
+        }
+         
+
+    }
+}
